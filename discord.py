6c5f34de61cb27a11e5e5e7c5b1c8720ab51a2b8
lemonade <taelemansst@gmail.com>
2018-06-08 22:25:42 +0200

    cleanup

--- discord.py
+++ discord.py
@@ -2,18 +2,9 @@ import requests
 import socket
 import re
 import json
-#
-#          LEMONADE'S
-#     VSV DISCORD POST BOT
-#    POST MESSAGES TO "BOT"
-#        CHANNEL (VSV)
-#
-#   CHANGE PARAMS TO LIKING
-#       HAVE FUN NERDS
-#
 
 #Webhook URL
-#-- best afblijven (tenzij ge dit gebruikt voor een andere server)
+#-- required
 url = ""
 
 #PARAMS
@@ -34,15 +25,15 @@ avatar_url = "https://newgateclocks.com/media/catalog/product/cache/10/image/9df
 tts="true"
 
 #FILE TO OPEN (ENTER PATH)
-#-- optional default empty (TODO -- werkt niet)
+#-- optional default empty (TODO -- doesn't work)
 filepath=""
     
-#EMBED LINKS (yt videos enzo denk ik)
-#-- optional default emptt
+#EMBED LINKS
+#-- optional default empty
 embeds=""
 
 
-#AFBLIJVEN -- BELANGRIJKE SHIT
+#parsing etc
 query = {}
 paramnames = ["username", "content", "avatar_url", "tts", "filepath", "embeds"]
 params = [username, content, avatar_url, tts, filepath, embeds]
@@ -59,7 +50,7 @@ if len(params[2]) == 0 or not re.match(httpr, avatar_url):
     query[paramnames[2]] = "https://showme1-9071.kxcdn.com/pics/profile/avatar/fbavatar_53b75406810298d982889daa383874cf.jpg"
 if len(params[3]) == 0 or (params[3] != "true" or params[3] != "false"):
     query[paramnames[3]] = "false"
-#url check
+#url checks
 if len(url) == 0:
     print("please provide webhook url")
     quit()
@@ -68,6 +59,7 @@ if re.match(pattern, query[paramnames[2]]) and query[paramnames[2]] != "https://
     print("Avatar URL OK")
 if re.match(httpr, url):
     print("Webhook URL OK")
+#posting
 try:
     r = requests.get(url)
     data = json.loads(r.text)
@@ -80,4 +72,4 @@ if requests.post(url, data=query):
     print("Message sent")
 else:
     print("Message failed to send. "+json.loads(requests.post(url, data=query).text)["message"])
-#TODO: modules en files
+#TODO: modules/options & files
